f9c6d20b156b2186f0abb1d4b53fc826dc00f9cb
Vandanalalli06 <vandanalalli06@gmail.com>
2022-08-24 23:52:58 +0530

    List of Rides using UserID

--- Day_30_CabVoiceGenerator/Day_30_CabVoiceGenerator/CabvoiceException.cs
+++ Day_30_CabVoiceGenerator/Day_30_CabVoiceGenerator/CabvoiceException.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace InvoiceGenerator
+namespace CabInvoiceGenerator
 {
-    public class CabInvoiceCustomException : Exception
+    //CabInvoiceException.cs
+
+    class CabInvoiceException : Exception
     {
-        public ExceptionType type;
-        public CabInvoiceCustomException(ExceptionType type, string message) : base(message)
-        {
-            this.type = type;
-        }
         public enum ExceptionType
         {
-            INVALID_ID_DISTANCE, INVALIDID_TIME, NULL_RIDES, INVALID_USER_ID, INVALIDID_RIDETYPE
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            NULL_RIDES,
+            INVALID_USER_ID
+        }
+
+        ExceptionType type;
+        public CabInvoiceException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
         }
     }
 }
