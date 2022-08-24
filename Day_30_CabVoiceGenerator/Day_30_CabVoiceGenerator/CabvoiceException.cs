8fcd346c7df8f35dd7290fabbc30ab3ee6a7400d
Vandanalalli06 <vandanalalli06@gmail.com>
2022-08-24 20:24:28 +0530

    Welcome to Cab Invoice Generator Program

---
+++ Day_30_CabVoiceGenerator/Day_30_CabVoiceGenerator/CabvoiceException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_30_CabVoiceGenerator
+{
+    internal class CabvoiceException
+    {
+        public class CabInvoiceCustomException : Exception
+        {
+            public ExceptionType type;
+            public CabInvoiceCustomException(ExceptionType type, string message) : base(message)
+            {
+                this.type = type;
+            }
+            public enum ExceptionType
+            {
+                INVALID_ID_DISTANCE, INVALIDID_TIME, NULL_RIDES, INVALID_USER_ID, INVALIDID_RIDETYPE
+            }
+        }
+    }
+}
+
+
---
+++ Day_30_CabVoiceGenerator/Day_30_CabVoiceGenerator/RideType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_30_CabVoiceGenerator
+{
+    public enum RideType { NORMAL, PREMIUM }
+}
+       
+    
+        
+
+    
+
